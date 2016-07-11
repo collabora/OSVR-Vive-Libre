edf65c00c2abf9bb49768d1d44781808943b1fe0
Lubosz Sarnecki <lubosz.sarnecki@collabora.co.uk>
2016-07-11 16:32:08 +0200

    vivectl: map some commands to functions.

--- tools/vivectl.cpp
+++ tools/vivectl.cpp
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <string>
-#include "openhmd.h"
-#include "platform.h"
 #include "vive.h"
 
 
@@ -54,22 +52,67 @@ vive_priv* init_hmd_context() {
     return (vive_priv*)ctx_openhmd->active_devices[0];
 }
 
+void free_hmd_context(vive_priv* priv) {
+    hid_close(priv->hmd_handle);
+    hid_close(priv->imu_handle);
+    hid_close(priv->watchman_dongle_handle);
+    hid_close(priv->lighthouse_sensor_handle);
+    free(priv);
+}
+
 void dump_controller() {
     vive_priv* priv = init_hmd_context();
     while(true)
         print_watchman_sensors(priv);
+    free_hmd_context(priv);
 }
 
 void dump_hmd_imu() {
     vive_priv* priv = init_hmd_context();
     while(true)
         print_imu_sensors(priv);
+    free_hmd_context(priv);
 }
 
 void dump_hmd_light() {
     vive_priv* priv = init_hmd_context();
     while(true)
         print_hmd_light_sensors(priv);
+    free_hmd_context(priv);
+}
+
+void send_hmd_on() {
+    int hret = 0;
+    vive_priv* priv = init_hmd_context();
+    printf("hmd on.\n");
+
+    // turn the display on
+    hret = hid_send_feature_report(priv->hmd_handle, vive_magic_power_on, sizeof(vive_magic_power_on));
+    printf("power on magic: %d\n", hret);
+    free_hmd_context(priv);
+}
+
+void send_hmd_off() {
+    int hret = 0;
+    vive_priv* priv = init_hmd_context();
+    printf("hmd off.\n");
+
+    // turn the display off
+    hret = hid_send_feature_report(priv->hmd_handle, vive_magic_power_off1, sizeof(vive_magic_power_off1));
+    printf("power off magic 1: %d\n", hret);
+
+    hret = hid_send_feature_report(priv->hmd_handle, vive_magic_power_off2, sizeof(vive_magic_power_off2));
+    printf("power off magic 2: %d\n", hret);
+
+    free_hmd_context(priv);
+}
+
+void send_controller_off() {
+    int hret = 0;
+    printf("controller off.\n");
+    vive_priv* priv = init_hmd_context();
+    hret = hid_send_feature_report(priv->watchman_dongle_handle, vive_controller_power_off, sizeof(vive_controller_power_off));
+    free_hmd_context(priv);
 }
 
 int main(int argc, char *argv[]) {
@@ -89,7 +132,17 @@ int main(int argc, char *argv[]) {
             }
         // send
         } else if (compare(argv[1], "send")) {
-            printf("send!\n");
+            if(compare(argv[2], "hmd-on")) {
+                send_hmd_on();
+            } else if(compare(argv[2], "hmd-off")) {
+                send_hmd_off();
+            } else if(compare(argv[2], "controller-off")) {
+                send_controller_off();
+            } else {
+                printf("Unknown argument %s\n", argv[2]);
+                print_usage();
+                return 0;
+            }
         } else {
             printf("Unknown argument %s\n", argv[1]);
             print_usage();
