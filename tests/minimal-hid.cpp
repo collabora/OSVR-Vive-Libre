1d966feba971c7525d526e0332f1ac745346e840
Lubosz Sarnecki <lubosz.sarnecki@collabora.co.uk>
2016-09-27 15:07:56 +0200

    tests: fix indent.

--- tests/minimal-hid.cpp
+++ tests/minimal-hid.cpp
@@ -6,34 +6,34 @@
 
 int main()
 {
-	wchar_t wstr[MAX_STR];
-	hid_device *handle;
+    wchar_t wstr[MAX_STR];
+    hid_device *handle;
 
-	// Initialize the hidapi library
+    // Initialize the hidapi library
     hid_init();
 
-	// Open the device using the VID, PID,
-	// and optionally the Serial number.
-	handle = hid_open(0x4d8, 0x3f, NULL);
+    // Open the device using the VID, PID,
+    // and optionally the Serial number.
+    handle = hid_open(0x4d8, 0x3f, NULL);
 
-	// Read the Manufacturer String
+    // Read the Manufacturer String
     hid_get_manufacturer_string(handle, wstr, MAX_STR);
-	wprintf(L"Manufacturer String: %s\n", wstr);
+    wprintf(L"Manufacturer String: %s\n", wstr);
 
-	// Read the Product String
+    // Read the Product String
     hid_get_product_string(handle, wstr, MAX_STR);
-	wprintf(L"Product String: %s\n", wstr);
+    wprintf(L"Product String: %s\n", wstr);
 
-	// Read the Serial Number String
+    // Read the Serial Number String
     hid_get_serial_number_string(handle, wstr, MAX_STR);
-	wprintf(L"Serial Number String: (%d) %s\n", wstr[0], wstr);
+    wprintf(L"Serial Number String: (%d) %s\n", wstr[0], wstr);
 
-	// Read Indexed String 1
+    // Read Indexed String 1
     hid_get_indexed_string(handle, 1, wstr, MAX_STR);
-	wprintf(L"Indexed String 1: %s\n", wstr);
+    wprintf(L"Indexed String 1: %s\n", wstr);
 
-	// Finalize the hidapi library
+    // Finalize the hidapi library
     hid_exit();
 
-	return 0;
+    return 0;
 }
